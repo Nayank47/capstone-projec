5867c2805b8ffc3b6670f1bee0a0f4a8d1ee75c2
Nayank47 <100655672+Nayank47@users.noreply.github.com>
2023-05-12 20:58:15 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Battle Royale Game</title>
+    <style>
+      /* Add styles for the page */
+      body {
+        font-family: Arial, sans-serif;
+        text-align: center;
+      }
+      h1 {
+        font-size: 36px;
+        margin-top: 50px;
+      }
+      .btn {
+        padding: 10px 20px;
+        background-color: green;
+        color: white;
+        border-radius: 5px;
+        text-decoration: none;
+        margin-top: 20px;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Battle Royale Game</h1>
+    <p>Join the ultimate survival battle!</p>
+    <a href="#" class="btn">Play Now</a>
+  </body>
+</html>
